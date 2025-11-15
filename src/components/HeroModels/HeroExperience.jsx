e342d44e835075f5a3344fa155e4bb7d61e4e873
Smith <smith@Smiths-MacBook-Air.local>
2025-11-15 02:40:29 -0800

    added abmbient Room lighting.

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,8 +1,9 @@
-import React, { use } from 'react'
+// import React, { use } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useMediaQuery } from 'react-responsive'
 import { Model as Room } from './Room.jsx'
+import HeroLights from '../HeroLights.jsx'
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({ query:'(max-width: 1024px)'});
@@ -10,10 +11,11 @@ const HeroExperience = () => {
     
     return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-        <ambientLight intensity={0.2}  color='#1a1a40'/>
-        <directionalLight position={[5, 5, 5,]} intensity={2} />
+        
         <OrbitControls enableZoom={!isTablet}
         enablePan={false} maxDistance={20} minDistance={5} minPolarAngle={Math.PI / 5} maxPolarAngle={Math.PI / 2} />
+       
+       <HeroLights/>
         <group scale={isMobile? 0.7 : 1} position={[0, -3.5, 0]}
             rotation={[0, -Math.PI / 4, 0]
         }>
